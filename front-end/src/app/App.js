707977e752385589fd345455d3ddf0a0e9f80853
indikaMax02 <indikamax02@gmail.com>
2022-07-16 23:26:36 +0530

    update

---
+++ front-end/src/app/App.js
@@ -0,0 +1,13 @@
+
+import React, {Component} from "react";
+import NavBar from "../components/navBar";
+import Home from "../pages/CustomerPages/home";
+
+
+function App() {
+  return (
+         <NavBar/>
+  );
+}
+
+export default App;
---
+++ front-end/src/components/navBar/style.js
@@ -0,0 +1,48 @@
+export const styleSheet={
+
+    container : {
+
+    },
+
+    topHeaderWrapper : {
+          display: 'flex',
+          flexDirection : 'row',
+          justifyContent: 'center',
+         alignItems : 'center',
+          height : '58px',
+          backgroundColor : 'rgba(23,20,23,0.94)'
+    },
+    contact_container : {
+            display : 'flex',
+            justifyContent : 'center',
+            backgroundColor: '#5476EC',
+            height: '100%',
+            width : '22%'
+    },
+    after : {
+        width: '0',
+        height: '0',
+       borderTop: '29px solid transparent',
+       borderLeft: '30px solid #5476EC',
+        borderBottom: '29px solid transparent'
+    },
+    header_right : {
+     /*   backgroundColor : 'blue',*/
+        flexGrow:'1',
+        height : '70%',
+        display : 'flex',
+    },
+    sm_icon :{
+        display : 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent :'center',
+        listStyle : 'none',
+
+
+
+    }
+
+
+
+}
